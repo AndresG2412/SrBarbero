11750e09aead90f1b434a8a3b728bf874be55d7a
Andres Gaviria <cgaviria930@gmail.com>
2025-07-02 22:28:46 -0500

    inicio Main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+'use client'
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image'
+import barba from './Images/barba.webp'
 
 export default function Home() {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
     return (
-        <div>
-            <h1>Bienvenido a Sr. Barbero</h1>
+        <div className='md:hidden relative bg-[url(./Images/bgfinal.webp)] bg-no-repeat bg-cover h-screen'>
+            <Image
+                src={barba}
+                alt='Barba'
+                className={`w-auto h-1/5 absolute object-cover ${isMounted ? 'animate-fadeInThenMove' : 'opacity-0'}`}
+                priority
+            />
         </div>
     )
 }
