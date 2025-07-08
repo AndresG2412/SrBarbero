8a54d01dd76ab1e18f5f8e4913db40e79d079dc3
Andres Gaviria <cgaviria930@gmail.com>
2025-07-07 23:20:39 -0500

    botones inicio

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -30,11 +30,18 @@ export default function Home() {
             />
 
             {/* colocar los botones */}
-            <div className='grid grid-cols-2 gap-8 w-10/12 absolute bottom-40 left-1/2 transform -translate-x-1/2'>
+            {/* <div className='grid grid-cols-2 gap-8 w-10/12 absolute bottom-40 left-1/2 transform -translate-x-1/2'>
                 <button className={`bg-red-200 h-44 rounded-lg ${showLetras ? 'animate-fadeInThenMoveBefore' : 'opacity-0'}`}>Boton1</button>
                 <button className={`bg-red-200 h-44 rounded-lg ${showLetras ? 'animate-fadeInThenMoveBefore' : 'opacity-0'}`}>Boton2</button>
                 <button className={`bg-red-200 h-44 rounded-lg ${showLetras ? 'animate-fadeInThenMoveBefore' : 'opacity-0'}`}>Boton3</button>
                 <button className={`bg-red-200 h-44 rounded-lg ${showLetras ? 'animate-fadeInThenMoveBefore' : 'opacity-0'}`}>Boton4</button>
+            </div> */}
+
+            <div className='grid grid-cols-1 gap-8 w-10/12 absolute btns-primary bottom-40 left-1/2 transform -translate-x-1/2'>
+                <button className={`bg-linear-to-b bg-[#D2981D] to-[#3C2E09] py-5 rounded-lg ${showLetras ? 'animate-fadeInThenMoveBefore' : 'opacity-0'}`}>Boton1</button>
+                <button className={`bg-linear-to-b bg-[#D2981D] to-[#3C2E09] py-5 rounded-lg ${showLetras ? 'animate-fadeInThenMoveBefore' : 'opacity-0'}`}>Boton2</button>
+                <button className={`bg-linear-to-b bg-[#D2981D] to-[#3C2E09] py-5 rounded-lg ${showLetras ? 'animate-fadeInThenMoveBefore' : 'opacity-0'}`}>Boton3</button>
+                <p className={`text-center  font-semibold text-lg w-5/6 mx-auto ${showLetras ? 'animate-fadeInThenMoveBefore' : 'opacity-0'}`}>"Barberos con muchos años de experiencia, trabajando con dedicacion para poder mejorar tu imagen ante el mundo"</p>
             </div>
 
             <p className={`mi-parrafo-con-imperial absolute w-full bottom-12 text-3xl text-center left-1/2 transform -translate-x-1/2 ${showLetras ? 'animate-fadeInThenMoveBefore' : 'opacity-0'}`}>Mas que un simple corte de pelo</p>        </div>
